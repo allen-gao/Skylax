e639c2987bf255b4da17a7bb7883b8c5ed34f721
allen-gao <allen001@live.ca>
2016-04-23 13:51:16 -0400

    angular-express seed

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-function init() {
-  var stage = new createjs.Stage("demoCanvas");
-  var circle = new createjs.Shape();
-  circle.graphics.beginFill("Crimson").drawCircle(0, 0, 50);
-  circle.x = 100;
-  circle.y = 100;
-  stage.addChild(circle);
-  createjs.Tween.get(circle, {loop: true})
-    .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
-    .to({alpha: 0, y: 75}, 500, createjs.Ease.getPowInOut(2))
-    .to({alpha: 0, y: 125}, 100)
-    .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
-    .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
-  createjs.Ticker.setFPS(60);
-  createjs.Ticker.addEventListener("tick", stage);
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+
+// development only
+if (env === 'development') {
+  app.use(express.errorHandler());
 }
+
+// production only
+if (env === 'production') {
+  // TODO
+}
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
