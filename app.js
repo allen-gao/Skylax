4afcc3c8ccbc718c34475dc9110766321d7af6fc
Allen Gao <allen001@live.ca>
2016-04-23 17:53:03 -0400

    coordinate converter

--- app.js
+++ app.js
@@ -13,4 +13,34 @@ function init() {
     .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
   createjs.Ticker.setFPS(60);
   createjs.Ticker.addEventListener("tick", stage);
+  console.log(convertXY(100, 100));
+
+  document.domain = 'omniweb.gsfc.nasa.gov';
+
+  $.ajax({
+  url: 'http://omniweb.gsfc.nasa.gov/cgi/models/planet.cgi',
+  data: {
+    'activity': 'retrieve',
+    'planet': '07',
+    'coordinate': '2',
+    'resolution': '001',
+start_year: '1998',
+start_day:'001',
+stop_year:'2013',
+stop_day:'365'
+  },
+  success: function(data){
+    console.log(data);
+  }
+});
+}
+
+function convertXY(long, lat) {
+  var radius = 100;
+  var x = radius * Math.cos(lat) * Math.cos(long);
+  var y = radius * Math.cos(lat) * Math.sin(long);
+  return {
+    x: x,
+    y: y
+  };
 }
